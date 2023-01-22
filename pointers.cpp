93463c910a09028f09344e21e48fe3253d8abc7d
AbhijeetRanja <ranjanabhijeet2407@gmail.com>
2023-01-22 23:11:00 +0530

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int a = 5;
+    // int *p = &a;
+
+    // cout << "Address of p is : " << p << endl;
+    // cout <<  "Address of P+1 is " << p+1 <<endl;
+    // //cout << &a << endl;
+    // cout << "Value at p : " << *p << endl; //Derefrenceing
+
+    // int b = 10;
+    // *p = b ;// the address in p will point towards b ?
+    // cout << "Address of p is : " << p << endl;
+    // cout << "Value at p : " << *p << endl; //Derefrenceing
+}
