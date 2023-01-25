9ed18bd1a5f5785f9be24eaa3229a057b3cb6f18
AbhijeetRanja <ranjanabhijeet2407@gmail.com>
2023-01-25 23:17:37 +0530

    templates

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+template<class>T;
+
+class Number{
+    T num;
+    
+    public:
+    Number(T n)
+    {
+        num=n;
+    }
+    T getNum()
+    {
+        num =n;
+    }
+};
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
